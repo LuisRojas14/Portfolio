7ed4309a8dd51de7541c576c8aeb82f25e16ad77
Luis Rojas <luisignacio1210@gmail.com>
2023-06-19 14:58:12 +0200

    adding css,and images to the home page

---
+++ src/components/HeroImg.js
@@ -0,0 +1,28 @@
+import "./HeroImgStyle.css";
+import React from "react";
+import IntroImg from "../assets/nasa-unsplash.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p> Hi, I am Luis Ignacio </p>
+        <h1> Junior Web Developer</h1>
+        <div>
+          <Link to="/project" className="btn">
+            Projects
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            Contact
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImg;
