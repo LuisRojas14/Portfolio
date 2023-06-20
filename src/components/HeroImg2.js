7e995fbf90a4cd7a5f40d6d6dee818d43c1dff55
Luis Rojas <luisignacio1210@gmail.com>
2023-06-20 11:51:23 +0200

    Add HeroImg2 and HeroImgStyles2 files

---
+++ src/components/HeroImg2.js
@@ -0,0 +1,15 @@
+import "./HeroImgStyle.css";
+import React from "react";
+
+const HeroImg2 = () => {
+  return (
+    <div className="hero-img">
+      <div className="heading">
+        <h1>this is Heading</h1>
+        <p>This is text</p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImg2;
