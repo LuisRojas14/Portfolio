4df76af83de9d1b007790b6352508424e308bb14
Luis Rojas <luisignacio1210@gmail.com>
2023-06-20 11:50:59 +0200

    Update Footer, About, Contact, and Project

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {
   FaHome,
+  FaPhone,
   FaEnvelope,
   FaInstagram,
   FaLinkedin,
   FaGithub,
+  // FaLocationArrow,
 } from "react-icons/fa";
 import "./FooterStyles.css";
 
@@ -14,43 +16,55 @@ const Footer = () => {
       <div className="footer-container">
         <div className="left">
           <div className="location">
-            <FaHome size={20} style={{ color: "#fff", marginRight: "2rem" }} />
-            <div>
-              <p>Hollestrasse 3</p>
-              <p>Dortmund</p>
-            </div>
-          </div>
-
-          <div className="email">
-            <FaEnvelope
+            <FaHome
               size={20}
               style={{ color: "#fff", marginRight: "2rem" }}
             />
-            <p>luisignacio1210@gmail.com</p>
+            <div>
+              <p>Dortmund, Germany</p>
+            </div>
+          </div>
+          <div className="phone">
+            <h4><FaPhone
+              size={20}
+              style={{ color: "#fff", marginRight: "2rem" }}
+            />
+             555-555-5555</h4>
+            
+          </div>
+          <div className="email">
+            <h4><FaEnvelope
+              size={20}
+              style={{ color: "#fff", marginRight: "2rem" }}
+            />
+            luisignacio1210@gmail.com</h4>
           </div>
         </div>
         <div className="right">
+          <h4> About me </h4>
+          <p>Hospitality industry professional with 14+ years' experience transitioning to web development. Seeking new opportunities to combine customer service expertise with a passion for technology.
+          </p>
           <div className="social-media">
             <a
-              href="https://www.instagram.com/your_instagram_profile"
+              href="https://www.instagram.com/maradonita_10/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaInstagram size={20} />
+              <FaInstagram size={30} style={{ color: "#fff", marginRight: "2rem" }}/>
             </a>
             <a
-              href="https://www.linkedin.com/in/your_linkedin_profile"
+              href="https://www.linkedin.com/in/luis-i-rojas/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaLinkedin size={20} />
+              <FaLinkedin size={30} style={{ color: "#fff", marginRight: "2rem" }}/>
             </a>
             <a
-              href="https://github.com/your_github_profile"
+              href="https://github.com/LuisRojas14"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaGithub size={20} />
+              <FaGithub size={30} style={{ color: "#fff", marginRight: "2rem" }} />
             </a>
           </div>
         </div>
