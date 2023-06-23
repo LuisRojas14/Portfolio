e67f7c2c1046a34c36691aee313bd2a9fb499c2a
Luis Rojas <luisignacio1210@gmail.com>
2023-06-23 08:01:58 +0200

    adding projects cards

--- src/components/WorkCard.js
+++ src/components/WorkCard.js
@@ -10,21 +10,54 @@ const WorkCard = () => {
       <div className="project-container">
         <div className="project-card">
           <img src={pro1} alt="photo" />
-          <h2 className="project-title"> Project Title</h2>
+          <h2 className="project-title"> Project Title 1</h2>
           <div className="pro-details">
-            <p> this is text</p>
+            <p> text  Lorem ipsum dolor sit amet. Cum aperiam maiores ea odio exercitationem est quia inventore aut provident enim et magnam dolorem! 33 vitae recusandae ab
+            </p>
             <div className="pro-btns">
               <NavLink to="url.com" className="btn">
                 View
               </NavLink>
-              <NavLink to="url.com" className="btn">
+              {/* <NavLink to="url.com" className="btn">
                 source
+              </NavLink> */}
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <img src={pro1} alt="photo" />
+          <h2 className="project-title"> Project Title 2</h2>
+          <div className="pro-details">
+            <p> text  Lorem ipsum dolor sit amet. Cum aperiam maiores ea odio exercitationem est quia inventore aut provident enim et magnam dolorem! 33 vitae recusandae ab
+            </p>
+            <div className="pro-btns">
+              <NavLink to="url.com" className="btn">
+                View
               </NavLink>
+              {/* <NavLink to="url.com" className="btn">
+                source
+              </NavLink> */}
+            </div>
+          </div>
+        </div>
+        <div className="project-card">
+          <img src={pro1} alt="photo" />
+          <h2 className="project-title"> Project Title 3</h2>
+          <div className="pro-details">
+            <p> text  Lorem ipsum dolor sit amet. Cum aperiam maiores ea odio exercitationem est quia inventore aut provident enim et magnam dolorem! 33 vitae recusandae ab
+            </p>
+            <div className="pro-btns">
+              <NavLink to="url.com" className="btn">
+                View
+              </NavLink>
+              {/* <NavLink to="url.com" className="btn">
+                source
+              </NavLink> */}
             </div>
           </div>
         </div>
       </div>
-    </div>
+      </div>
   );
 };
 
