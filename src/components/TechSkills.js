641fedd7075eeabbfcff3e075a4cfecf1ce3fcad
Luis Rojas <luisignacio1210@gmail.com>
2023-07-24 17:15:23 +0200

    adding skill images

--- src/components/TechSkills.js
+++ src/components/TechSkills.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import expressLogo from "../assets/express.png";
+import mongoDBLogo from "../assets/mongoDB.png";
+import { Container, Card } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faReact,
@@ -8,47 +10,45 @@ import {
   faCss3,
   faBootstrap,
   faNodeJs,
+  faGithub,
+  faPython,
+  faGit,
 } from "@fortawesome/free-brands-svg-icons";
-
 import "./TechSkills.css";
 
-const TechSkillsComponent = () => {
-  const techSkills = [
-    // frontend
-    { name: " HTML5", icon: faHtml5 },
-    { name: " CSS3", icon: faCss3 },
-    { name: " JavaScript", icon: faJs },
-    { name: " React JS", icon: faReact },
-    { name: " Bootstrap", icon:  faBootstrap },
-      //backend
-    { name: " Node.js", icon: faNodeJs },
-   
-
-   
+const TechSkills = () => {
+  const skills = [
+    { name: "HTML5", icon: faHtml5 },
+    { name: "CSS3", icon: faCss3 },
+    { name: "JavaScript", icon: faJs },
+    { name: "React JS", icon: faReact },
+    { name: "Bootstrap", icon: faBootstrap },
+    { name: "Node.js", icon: faNodeJs },
+    { name: "GitHub", icon: faGithub },
+    { name: "Python", icon: faPython },
+    { name: "Git", icon: faGit },
+    { name: "Express.js", icon: expressLogo },
+    { name: "MongoDb", icon: mongoDBLogo },
   ];
 
   return (
     <Container>
       <h2 className="tech">My Tech Skills</h2>
-      <Row> 
-        {techSkills.map((skill, index) => (
-          <Col key={index} sm={10} md={4} lg={3}>
-            <Card className="skill-card">
-              <Card.Body>
-                <Card.Title className="skill-card-title">
-                  <FontAwesomeIcon
-                    icon={skill.icon}
-                    className={`skill-logo ${skill.name.toLowerCase()}`}
-                  />
-                  {skill.name}
-                </Card.Title>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+      <Card className="skill-card">
+        <Card.Body className="skill-card-body">
+          {skills.map((skill, index) => (
+            <div key={index} className="skill">
+              <FontAwesomeIcon
+                icon={skill.icon}
+                className={`skill-logo ${skill.name.toLowerCase()}`}
+              />
+              {skill.name}
+            </div>
+          ))}
+        </Card.Body>
+      </Card>
     </Container>
   );
 };
 
-export default TechSkillsComponent;
+export default TechSkills;
